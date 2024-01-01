5ab15a913e0a07f28f004950ba34e2938246348e
gleb <webrushgithub@gmail.com>
2024-01-01 15:53:06 +0700

    Сделал начало бота(есть баг)

--- logic.py
+++ logic.py
@@ -1,12 +1,15 @@
-from translate import Translator
-import requests
 from collections import defaultdict
 
-qwestions = {'Как тебя зовут' : "Я супер-крутой-бот и мое ппредназначение помогать тебе!",
-             "Сколько тебе лет" : "Это слишком философский вопрос"}
+import requests
+import speech_recognition as sr
+from translate import Translator
 
-class TextAnalysis():   
-    
+questions = {'Как тебя зовут': "Я супер-крутой-бот и мое предназначение помогать тебе!",
+             "Сколько тебе лет": "Это слишком философский вопрос"}
+
+
+class TextAnalysis():
+
     memory = defaultdict(list)
 
     def __init__(self, text, owner):
@@ -16,19 +19,18 @@ class TextAnalysis():
         self.text = text
         self.translation = self.__translate(self.text, "ru", "en")
 
-        if self.text in qwestions.keys():
-            self.response = qwestions[self.text]
+        if self.text in questions.keys():
+            self.response = questions[self.text]
         else:
-            self.response = self.get_answer() 
+            self.response = self.get_answer()
 
-    
     def get_answer(self):
         res = self.__translate(self.__deep_pavlov_answer(), "en", "ru")
         return res
 
     def __translate(self, text, from_lang, to_lang):
         try:
-            translator= Translator(from_lang=from_lang, to_lang=to_lang)
+            translator = Translator(from_lang=from_lang, to_lang=to_lang)
             translation = translator.translate(text)
             return translation
         except:
@@ -37,10 +39,23 @@ class TextAnalysis():
     def __deep_pavlov_answer(self):
         try:
             API_URL = "https://7038.deeppavlov.ai/model"
-            data = {"question_raw": [ self.translation ]}
+            data = {"question_raw": [self.translation]}
             res = requests.post(API_URL, json=data).json()
             res = res[0][0]
         except:
             res = "I don't know how to help"
         return res
 
+
+class VoiceTranscriber(TextAnalysis):
+    def __init__(self, path, owner):
+        self.path = path
+        self.text = self.__recognize()
+        super().__init__(self.text, owner)
+
+    def __recognize(self):
+        recognizer = sr.Recognizer()
+        with sr.AudioFile(self.path) as source:
+            audio = recognizer.record(source)
+        text = recognizer.recognize_google(audio)
+        return text
--- main.py
+++ main.py
@@ -10,36 +10,80 @@ import os
 
 bot = telebot.TeleBot(token)
 
-def gen_markup_for_text():
-        markup = InlineKeyboardMarkup()
-        markup.row_width = 1
 
-        markup.add(InlineKeyboardButton('Получить ответ', callback_data='text_ans'),
-                   InlineKeyboardButton('Перевести сообщение', callback_data='text_translate'))
-        
-        return markup
+def gen_markup_for_text():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+
+    markup.add(InlineKeyboardButton('Получить ответ', callback_data='text_ans'),
+               InlineKeyboardButton('Перевести сообщение', callback_data='text_translate'))
+
+    return markup
+
+
+def gen_markup_for_voice():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1
+
+    markup.add(InlineKeyboardButton('Транскрибировать аудио', callback_data='voice_transcribe'),
+               InlineKeyboardButton(
+                   'Получить ответ', callback_data='voice_ans'),
+               InlineKeyboardButton('Перевести сообщение', callback_data='voice_translate'))
+
+    return markup
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-    
+
     if "text" in call.data:
         obj = TextAnalysis.memory[call.from_user.username][-1]
 
         if call.data == "text_ans":
             bot.send_message(call.message.chat.id, obj.response)
-                
+
         elif call.data == "text_translate":
             bot.send_message(call.message.chat.id,  obj.translation)
+
+    elif "voice" in call.data:
+        obj = VoiceTranscriber.memory[call.from_user.username][-1]
+        if call.data == "voice_transcribe":
+            bot.send_message(call.message.chat.id, obj.text)
+        elif call.data == "voice_ans":
+            # bot.send_message(call.message.chat.id, obj)
+            pass
+        elif call.data == "voice_translate":
+            pass
+            # bot.send_message(call.message.chat.id, obj)
+
+
+@bot.message_handler(content_types=['voice'])
+def handle_voice(message):
+    filename = str(uuid.uuid4())
+    file_name_full = 'voice/'+filename+'.ogg'
+    file_name_full_converted = 'ready/'+filename+".wav"
+    file_info = bot.get_file(message.voice.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+
+    with open(file_name_full, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    os.system("ffmpeg -i "+file_name_full+"  "+file_name_full_converted)
+
+    VoiceTranscriber(file_name_full_converted, message.from_user.username)
+
+    os.remove(file_name_full)
+    os.remove(file_name_full_converted)
+
+    bot.send_message(message.chat.id, "Я получил твое голосовое сообщение! Что ты хочешь с ним сделать?",
+                     reply_markup=gen_markup_for_voice())
 
 
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-
-    bot.send_chat_action(message.chat.id, 'typing')
+    bot.send_chat_action(message.chat.id, "typing")
     TextAnalysis(message.text, message.from_user.username)
-    bot.send_message(message.chat.id, "Я получил твое сообщение! Что ты хочешь с ним сделать?", reply_markup=gen_markup_for_text())
-
+    bot.send_message(message.chat.id, "Я получил твое сообщение! Что ты хочешь с ним сделать?",
+                     reply_markup=gen_markup_for_text())
 
 
 bot.infinity_polling(none_stop=True)
